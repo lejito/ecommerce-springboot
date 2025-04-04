18f05da46528620afab0fe3632fd94a6539fc24c
Lejitop <alejocrrs@gmail.com>
2025-04-03 19:06:59 -0500

    Refactor: rename ItemNoEcontrado to ItemNoEncontrado for consistency

--- complete/src/main/java/com/dsfyr/ecommerce/Error/ItemNoEcontrado.java
+++
@@ -1,8 +0,0 @@
-package com.dsfyr.ecommerce.Error;
-
-public class ItemNoEcontrado extends CustomError {
-    public ItemNoEcontrado(String message) {
-        super(message);
-    }
-
-}
---
+++ complete/src/main/java/com/dsfyr/ecommerce/Error/ItemNoEncontrado.java
@@ -0,0 +1,8 @@
+package com.dsfyr.ecommerce.Error;
+
+public class ItemNoEncontrado extends CustomError {
+    public ItemNoEncontrado(String message) {
+        super(message);
+    }
+
+}
